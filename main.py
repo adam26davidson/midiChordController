889e2798a1169fd5614959a4ae734696e8140ec1
Adam Davidson <adamdavidson@Adams-MBP.home>
2021-08-30 16:32:25 -0400

    pushing lots of updates

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from display.display import Display
 from midiShock import MidiShock
 from dualShock import DualShock
 import threading
+import asyncio
 import time
 
 display = Display()
 midiShock = MidiShock(display)
-dualShock = DualShock(midiShock)
+
 sides = ["left", "none", "right"]
 
 def test():
-  midiShock.playChord("ex")
-  midiShock.setKey(2)
-  #midiShock.setSpread(6)
-  midiShock.setAlternate(True)
-  #midiShock.setModulation("left")
-  midiShock.setSecondary("left")
-  for i in range(1, 20):
-    time.sleep(0.5)
-    midiShock.setSpread(i)
+  dualShock = DualShock(midiShock)
+  loop = asyncio.get_event_loop()
+  loop.run_forever()
+
+  # midiShock.playChord("ex")
+  # midiShock.setKey(2)
+  # midiShock.setSpread(6)
+  # midiShock.setAlternate(True)
+  # midiShock.setModulation("left")
+  # midiShock.setSecondary("left")
+  # for i in range(1, 20):
+  #   time.sleep(0.5)
+  #   midiShock.setSpread(i)
 
 
-# t = threading.Thread(target=test)
-# t.daemon = True
-# t.start()
+t = threading.Thread(target=test)
+t.daemon = True
+t.start()
 
 display.root.mainloop()
 
