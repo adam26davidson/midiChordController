d913a628b5b8e858d88d66e88c65140ec3c2601c
Adam Davidson <adamdavidson@Adams-MBP.home>
2021-08-30 16:07:54 -0400

    pushing lots of updates

--- dualShock.py
+++ dualShock.py
@@ -1,5 +1,4 @@
 import evdev
-from evdev import InputDevice, categorize, ecodes
 import asyncio
 
 class DualShock:
@@ -8,17 +7,17 @@ class DualShock:
     self.values = {}
     self.ranges = {
       "gyroXRange": {"top": -8050, "bottom": 8050},
-      0:{"min":0, "max":0},
-      1:{"min":0, "max":0},
-      2:{"min":0, "max":0},
-      3:{"min":0, "max":0},
-      4:{"min":0, "max":0},
-      5:{"min":0, "max":0}
+      0:{"min":0, "max":1},
+      1:{"min":0, "max":1},
+      2:{"min":0, "max":1},
+      3:{"min":0, "max":1},
+      4:{"min":0, "max":1},
+      5:{"min":0, "max":1}
     }
     if (evdev.list_devices().count('/dev/input/event1') == 1):
-      self.motion = InputDevice('/dev/input/event1')
-      self.touch = InputDevice('/dev/input/event0')
-      self.buttons = InputDevice('/dev/input/event2')
+      self.motion = evdev.InputDevice('/dev/input/event1')
+      self.touch = evdev.InputDevice('/dev/input/event0')
+      self.buttons = evdev.InputDevice('/dev/input/event2')
 
       asyncio.ensure_future(self.motionLoop(sliders))
       asyncio.ensure_future(self.buttonsLoop())
@@ -31,8 +30,6 @@ class DualShock:
           self.ranges[event.code]["min"] = event.value
       if event.value > self.ranges[event.code]["max"]:
           self.ranges[event.code]["max"] = event.value
-      if event.code == 0:
-        print(event.value)
       sliders.positionThumb(self.values, self.ranges, event.code)
   
   async def buttonsLoop(self):
