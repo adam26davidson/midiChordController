70506b66e926bcd51265bec720d4bfdce5dd3536
Adam Davidson <adamdavidson@Adams-MBP.home>
2021-08-31 13:21:46 -0400

    pushing lots of updates

--- dualShockTest.py
+++ dualShockTest.py
@@ -39,7 +39,7 @@ class DualShock:
         print(evdev.categorize(event))
         print(event)
       elif event.type == evdev.ecodes.EV_ABS:
-        if event.code == 0:
+        if event.code == 1:
           print(event)
 
   async def touchLoop(self):
