ff73e904ca127e0c9a3efc12adaf4ae7a08938c3
Adam Davidson <adamdavidson@Adams-MBP.home>
2021-09-02 12:17:26 -0400

    pushing lots of updates

--- display/chordDisplay.py
+++ display/chordDisplay.py
@@ -21,7 +21,7 @@ class ChordDisplay(tk.Canvas):
     self.master = master
     self.scale = [0, 2, 4, 5, 7, 9, 11]
     self.positions, self.notes = self.createNotes()
-    self.pack(side="bottom", pady=20)
+    self.pack(side="right", padx=20)
   
   def createNotes(self):
     positions = []
