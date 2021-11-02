37e6fa09856afc919dae29834eb74fe5f20daa3d
Adam Davidson <adam26davidson@gmail.com>
2021-11-02 18:27:14 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -13,10 +13,12 @@ class TextDisplay(tk.Frame):
     self.master = master
     settingFont = ("sans serif", 20)
     controllerFont = ("sans serif", 15)
-    tk.Label(self, text="setting: ", bg=self.bgColor).pack(side="left")
+
+    tk.Label(self, text="setting: ", fg="#ffffff", bg=self.bgColor).pack(side="left")
     self.setting = tk.Label(self, text="Loading...", bg=self.bgColor, width=self.width, fg="#ffffff", justify="left", font=settingFont)
     self.setting.pack(side="left", pady=(0, 10))
-    tk.Label(self, text="controller: ", bg=self.bgColor).pack(side="left")
+    
+    tk.Label(self, text="controller: ", fg="#ffffff", bg=self.bgColor).pack(side="left")
     self.controller = tk.Label(self, text="Not Connected", bg=self.bgColor, width=self.width, fg="#ffffff", justify="left", font=controllerFont)
     self.controller.pack(side="left", pady=(0, 10))
 
