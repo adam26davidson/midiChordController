7539c6952f53d61e597dd132493987e1f5683536
Adam Davidson <adam26davidson@gmail.com>
2021-11-02 14:13:41 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -12,7 +12,7 @@ class TextDisplay(tk.Frame):
   controllerText = "No Controller Connected"
 
   def __init__(self, master=None):
-    super().__init__(master, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg=self.bgColor)
+    super().__init__(master, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg="#ffffff")
     self.master = master
     self.pack(side="top", padx=(20,20), pady=(20,20))
     self.setting = tk.Label(self, textvariable=self.settingText, width=100, height=40, bg=self.bgColor, fg="#ffffff", justify="left")
