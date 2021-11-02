04502b8f22c01f27f74a7802340233e84eb247a4
Adam Davidson <adam26davidson@gmail.com>
2021-11-02 14:04:28 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -14,10 +14,10 @@ class TextDisplay(tk.Frame):
   def __init__(self, master=None):
     super().__init__(master, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg=self.bgColor)
     self.master = master
-    self.setting = tk.Label(self, textvariable=self.settingText, width=200, height=20, bg=self.bgColor)
-    self.setting.pack(side="top", pady=(0, 10))
-    self.controller = tk.Label(self, textvariable=self.controllerText, width=200, height=20, bg=self.bgColor)
-    self.controller.pack(side="top", pady=(0, 10))
+    self.setting = tk.Label(self, textvariable=self.settingText, width=200, height=20, bg=self.bgColor, fg="#ffffff")
+    self.setting.pack(side="left", pady=(0, 10))
+    self.controller = tk.Label(self, textvariable=self.controllerText, width=200, height=20, bg=self.bgColor, fg="#ffffff")
+    self.controller.pack(side="left", pady=(0, 10))
 
     self.pack(side="top", padx=(20,20), pady=(20,20))
 
