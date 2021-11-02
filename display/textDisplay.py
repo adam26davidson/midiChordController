b32cbdb605153b623a59701b536a486bdacf6206
Adam Davidson <adam26davidson@gmail.com>
2021-11-02 14:09:12 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from constants import *
 
 class TextDisplay(tk.Frame):
-  width = 200
+  width = 100
   height = 200
 
   bgColor = "#000000"
@@ -14,11 +14,12 @@ class TextDisplay(tk.Frame):
   def __init__(self, master=None):
     super().__init__(master, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg=self.bgColor)
     self.master = master
-    self.setting = tk.Label(self, textvariable=self.settingText, width=200, height=20, bg=self.bgColor, fg="#ffffff")
-    self.setting.pack(side="left", pady=(0, 10))
-    self.controller = tk.Label(self, textvariable=self.controllerText, width=200, height=20, bg=self.bgColor, fg="#ffffff")
-    self.controller.pack(side="left", pady=(0, 10))
-
     self.pack(side="top", padx=(20,20), pady=(20,20))
+    self.setting = tk.Label(self, textvariable=self.settingText, width=100, height=40, bg=self.bgColor, fg="#ffffff", justify="left")
+    self.setting.pack(pady=(0, 10))
+    self.controller = tk.Label(self, textvariable=self.controllerText, width=100, height=40, bg=self.bgColor, fg="#ffffff", justify="left")
+    self.controller.pack(pady=(0, 10))
+
+    
 
 
