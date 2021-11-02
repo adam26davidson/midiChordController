4b2eff162689ed7f8cc7816b352f9a90fed1d77f
Adam Davidson <adam26davidson@gmail.com>
2021-11-02 18:33:45 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -11,20 +11,20 @@ class TextDisplay(tk.Frame):
   def __init__(self, master=None):
     super().__init__(master, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg=self.bgColor)
     self.master = master
-    settingFont = ("sans serif", 20)
-    controllerFont = ("sans serif", 15)
+    settingFont = ("sans serif", 18)
+    controllerFont = ("sans serif", 14)
 
     self.settingFrame = tk.Frame(self)
-    self.settingFrame.pack(side="top")
+    self.settingFrame.pack(side="top", bg=self.bgColor)
     tk.Label(self.settingFrame, text="setting: ", fg="#ffffff", bg=self.bgColor).pack(side="left")
     self.setting = tk.Label(self.settingFrame, text="Loading...", bg=self.bgColor, width=self.width, fg="#ffffff", justify="left", font=settingFont)
-    self.setting.pack(side="left", pady=(0, 10))
+    self.setting.pack(side="left")
 
     self.controllerFrame = tk.Frame(self)
-    self.controllerFrame.pack(side="top")
+    self.controllerFrame.pack(side="top", bg=self.bgColor)
     tk.Label(self.controllerFrame, text="controller: ", fg="#ffffff", bg=self.bgColor).pack(side="left")
     self.controller = tk.Label(self.controllerFrame, text="Not Connected", bg=self.bgColor, width=self.width, fg="#ffffff", justify="left", font=controllerFont)
-    self.controller.pack(side="left", pady=(0, 10))
+    self.controller.pack(side="left")
 
     self.pack(side="top", padx=(20,20), pady=(20,20))
 
