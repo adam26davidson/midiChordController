53db1ea8b0abdb29882ff7f9eca5e537080e1658
Adam Davidson <adam26davidson@gmail.com>
2021-11-03 16:53:38 -0400

    testing

--- display/textDisplay.py
+++ display/textDisplay.py
@@ -32,7 +32,7 @@ class TextDisplay(tk.Frame):
     print(controllerIcon)
     self.controllerFrame = tk.Frame(self, bg=self.bgColor)
     self.controllerFrame.pack(side="top")
-    tk.Label(self.controllerFrame, image=controllerIcon, bg=self.bgColor).pack(side="left")
+    tk.Label(self.controllerFrame, image=controllerIcon).pack(side="left")
     self.controller = tk.Label(self.controllerFrame, text="Not Connected", bg=self.bgColor, fg=self.color, font=mediumFont)
     self.controller.pack(side="left")
 
