c0ced6f059429127fa7ff9f27f0700d286f73d8f
Adam Davidson <adam26davidson@gmail.com>
2021-11-30 16:41:49 -0500

    testing new keyboard

--- display/keyboard.py
+++ display/keyboard.py
@@ -59,8 +59,8 @@ class Keyboard(tk.Canvas):
             outline=self.chordColor, 
             width=self.keyOutlineWidth),
           "center": {
-            "x": xL + self.smallRadius, 
-            "y": yT + + self.smallRadius
+            "x": xL + self.keyDiameter / 2, 
+            "y": yT + self.keyDiameter / 2
           }
         }
       keys[i] = key
