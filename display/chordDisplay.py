de2d70cd8ae1c64f4a0214587fbe7b9333a2ba65
Adam Davidson <adam26davidson@gmail.com>
2021-12-02 15:36:08 -0500

    testing new keyboard

--- display/chordDisplay.py
+++ display/chordDisplay.py
@@ -239,8 +239,8 @@ class ChordDisplay(tk.Canvas):
 
   def setBassShadow(self, note):
     note = self.convertNote(note)
-    color = COLORS["chor"]
-    if note == self.root: color = COLORS["roo"]
+    color = COLORS["chord"]
+    if note == self.root: color = COLORS["root"]
     self.setBassPositionAndRadius(note, self.bassNoteShadowRadius)
     self.setBassWidth(self.bassOutlineShadowWidth)
     self.setBassOutlineColor(color)
