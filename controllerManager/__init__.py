8d32bcde5b66319d71024c1598809c7ac3110cd2
Adam Davidson <adam26davidson@gmail.com>
2021-12-09 13:09:05 -0500

    started restructuring controller manager folder and writing controller manager class

---
+++ controllerManager/__init__.py
@@ -0,0 +1,29 @@
+from .controllers import controllerClasses
+import asyncio
+
+class controllerManager():
+  def __init__(self):
+    self.controllerClasses = controllerClasses
+    self.connectedControllers = {}
+    self.subscriberCallbacks = []
+
+  def subscribe(self, callBack):
+    self.subscriberCallbacks.append(callBack)
+
+  def sendEvent(self, event):
+    for callBack in self.subscriberCallbacks:
+      callBack(event)
+
+
+  async def searchForControllers(display):
+    foundController = False
+    connectedController = None
+    while (not foundController):
+      for Controller in controllers:
+        foundController = Controller.checkIfConnected()
+        if foundController:
+          connectedController = Controller(display)
+          connectedController.start()
+          break
+        await asyncio.sleep(0.25)
+      
---
+++ controllerManager/controllers/__init__.py
@@ -0,0 +1,7 @@
+from .DualShock4 import DualShock4
+from .Wired360Controller import Wired360Controller
+
+controllerClasses = [
+  DualShock4, 
+  Wired360Controller
+]
