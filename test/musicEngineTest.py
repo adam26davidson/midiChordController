9262f8a227daa54d5e4122c65da9a54eb08bf61a
Adam Davidson <adam26davidson@gmail.com>
2021-12-17 17:39:59 -0500

    worked on musicEngine overhaul

--- test/musicEngineTest.py
+++ test/musicEngineTest.py
@@ -11,16 +11,13 @@ def musicEngineTest():
     if 'value' not in event.keys():
       print(event['name'])
 
-  def handleStateChange():
-      print('STATE UPDATE')
-      controllers = store.get_state()['controllerManager']['controllers']
-      waiting = store.get_state()['controllerManager']['waitingForConnection']
-      print([c['name'] for c in controllers])
-      print('waitingForConnection: ' + str(waiting))
+  # def handleStateChange():
+  #   controllers = store.get_state()['controllerManager']['controllers']
+  #   waiting = store.get_state()['controllerManager']['waitingForConnection']
 
   c.subscribe(m.controllerEventHandler)
   c.subscribe(handleControllerEvent)
-  store.subscribe(handleStateChange)
+  # store.subscribe(handleStateChange)
 
   m.start()
   c.start()
