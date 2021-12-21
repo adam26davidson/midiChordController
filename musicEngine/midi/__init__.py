e478d0c18d4e0ab57ff47124f8a31027f4c548fc
Adam Davidson <adam26davidson@gmail.com>
2021-12-21 09:59:36 -0500

    testing rtp midi

--- musicEngine/midi/__init__.py
+++ musicEngine/midi/__init__.py
@@ -113,6 +113,7 @@ class Midi():
             interval = intervals[j],
             channel = noteChannel
           ))
+      print('playingChordNotes: {playingChordNotes}')
 
     elif type == 'off':
       if player == 'chord':
