bd629294b038d5403783dbcf69b71554cf8c1451
Adam Davidson <adam26davidson@gmail.com>
2022-02-13 13:39:14 -0500

    trying to fix the lingering notes

--- controllerManager/controllers/DualShock4/info.py
+++ controllerManager/controllers/DualShock4/info.py
@@ -250,7 +250,7 @@ info = {
       0: {
         "name": "TOUCHPAD_X",
         "type": "ANALOG",
-        "range": {"top": 1919, "bottom": 0},
+        "range": {"top": 0, "bottom": 1919},
         "events":{ 
           "value": "TOUCHPAD_X_UPDATE"
         },
