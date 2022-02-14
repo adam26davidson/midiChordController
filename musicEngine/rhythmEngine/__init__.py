7e1efce1180fc55abfba85381dbd64902dc9d4f6
Adam Davidson <adam26davidson@gmail.com>
2022-02-14 10:19:33 -0500

    trying to fix the lingering notes

--- musicEngine/rhythmEngine/__init__.py
+++ musicEngine/rhythmEngine/__init__.py
@@ -5,16 +5,13 @@ import time
 class RhythmEngine():
   def __init__(self):
     self.callbacks = []
+    self.scheduledNotesLock = asyncio.Lock()
     self.state = {
       'strumMode': 'random', # 'random', 'regular', 'off'
       'strumInterval': 0.1, # time beween notes or spread of distribution
       'strumOrder' : 'down', # 'up', 'down', or 'random'
-      'scheduledMessages': [],
-      'scheduledMessageLocked': False
+      'scheduledNotes': []
     }
-
-  def start(self):
-    asyncio.ensure_future(self.__scheduledMessageLoop())
 
   def subscribe(self, callback):
     self.callbacks.append(callback)
@@ -26,7 +23,7 @@ class RhythmEngine():
   def handleMessage(self, message):
     if message['player'] == 'chord':
       if message['type'] == 'off':
-        self.__handleChordOff(message['notes'])
+        asyncio.ensure_future(self.__handleChordOff(message['notes']))
       elif message['type'] == 'on':
         self.__handleChordOn(message['notes'])
     elif message['player'] == 'bass':
@@ -42,24 +39,6 @@ class RhythmEngine():
   def __sendMessage(self, message):
     for callback in self.callbacks:
       callback(message)
-
-  async def __scheduledMessageLoop(self):
-    while True:
-      if not self.state['scheduledMessageLocked']:
-        self.state['scheduledMessageLocked'] = True
-        messagesToKeep = []
-        for message in self.state['scheduledMessages']:
-          if message['playAt'] <= time.time():
-            self.__sendMessage({
-              'note': message['note'],
-              'type': message['type'],
-              'player': message['player']
-            })
-          else:
-            messagesToKeep.append(message)
-        self.state['scheduledMessages'] = messagesToKeep
-        self.state['scheduledMessageLocked'] = False
-      await asyncio.sleep(0)
 
   def __getRandomIntervals(self, n):
     values = random.normal(
@@ -82,8 +61,14 @@ class RhythmEngine():
       intervals = random.permutation(intervals) if self.state['strumOrder'] == 'random' else intervals
     return intervals
   
-  def __scheduleMessage(self, note):
-    self.state['scheduledMessages'].append(note)
+  async def __scheduleMessage(self, message, delay):
+    async with self.scheduledNotesLock:
+      self.state['scheduledNotes'].append(message['note'])
+    await asyncio.sleep(delay)
+    async with self.scheduledNotesLock:
+      if message['note'] in self.state['scheduledNotes']:
+        self.__sendMessage(message)
+        self.state['scheduledNotes'].remove(message['note'])
 
   def __handleChordOn(self, notes):
       self.state['scheduledNotes'] = []
@@ -96,25 +81,18 @@ class RhythmEngine():
           self.__sendMessage({'note': note, 'type': 'on', 'player': 'chord'})
         else:
           j = i if self.state['strumOrder'] != 'down' else (len(notes) - 1) -i
-          playAt = intervals[j] + time.time()
-          self.__scheduleMessage({'note': note, 'type': 'on', 'player': 'chord', 'playAt': playAt})
+          message = {'note': note, 'type': 'on', 'player': 'chord'}
+          asyncio.ensure_future(self.__scheduleMessage(message, intervals[j]))
   
-  def __handleChordOff(self, notes):
+  async def __handleChordOff(self, notes):
     self.state['scheduledMessageLocked'] = True
     for note in notes:
-      # remove scheduled message
-      messagesToKeep = []
-      for scheduledMessage in self.state['scheduledMessages']:
-        if not scheduledMessage['note'] == note: 
-          messagesToKeep.append(scheduledMessage)
-      self.state['scheduledMessages'] = messagesToKeep
+      # remove scheduled note
+      async with self.scheduledNotesLock:
+        self.state['scheduledNotes'].remove(note)
 
       #send note off
-      message = {
-        'note': note,
-        'type': 'off',
-        'player': 'chord',
-      }
+      message = {'note': note,'type': 'off','player': 'chord'}
       self.__sendMessage(message)
     self.state['scheduledMessageLocked'] = False
   
