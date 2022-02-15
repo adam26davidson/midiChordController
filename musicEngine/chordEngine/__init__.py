3e732ed83c73f0c6b319f58e32908457f33334d7
Adam Davidson <adam26davidson@gmail.com>
2022-02-15 15:27:57 -0500

    trying to fix the lingering notes

--- musicEngine/chordEngine/__init__.py
+++ musicEngine/chordEngine/__init__.py
@@ -157,8 +157,9 @@ class ChordEngine:
         store.dispatch(actions.stopChord())
         self.state['playingChordNotes'] = []
         self.state['chordIsPlaying'] = False
-        for button, state in self.state['chordButtonStates'].items():
-          if state: self.playChord(button)
+        for b, isOn in self.state['chordButtonStates'].items():
+          if isOn: 
+            self.playChord(b)
       
   def stopBass(self, buttonUp=True):
     if not buttonUp or not self.state['hold']:
