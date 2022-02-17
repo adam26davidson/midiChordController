c762160e621e9c55e2a6b71ff8feeae7ed5b36b7
Adam Davidson <adam26davidson@gmail.com>
2022-02-17 16:18:15 -0500

    fixing the dissapearing notes when no strum is used

--- musicEngine/rhythmEngine/__init__.py
+++ musicEngine/rhythmEngine/__init__.py
@@ -7,7 +7,7 @@ class RhythmEngine():
     self.callbacks = []
     self.scheduledNotesLock = asyncio.Lock()
     self.state = {
-      'strumMode': 'on', # 'random', 'regular', 'off'
+      'strumMode': 'random', # 'random', 'regular', 'off'
       'strumInterval': 0.05, # time beween notes or spread of distribution
       'strumOrder' : 'random', # 'up', 'down', or 'random'
       'scheduledNotes': []
