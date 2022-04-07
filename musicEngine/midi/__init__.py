d8f2e759205e0f17b29b6f485ec1b8b78bed5310
Rikitaro Suzuki <rsuzuki@Rikitaros-Mac-mini.local>
2022-04-07 15:24:40 -0400

    Added autoconnect for midi too, have not tested yet

--- musicEngine/midi/__init__.py
+++ musicEngine/midi/__init__.py
@@ -186,6 +186,24 @@ class Midi():
           channelCommand = self.__combineCommandAndChannel(CONTROL_CHANGE, channel)
           self.midiOut.send_message([channelCommand, cc, val])  
         self.state['lastSentCCValues'][cc] = val
+<<<<<<< HEAD
+=======
+
+  async def __loop(self):
+    while True:
+      if self.midiOut.is_port_open:
+        self.__sendAftertouch()
+        self.__sendCCValues()
+      else:
+        self.__reconnect()
+      await asyncio.sleep(MIDI_STEP)
+
+  def __reconnect(self):
+    self.availableOutputPorts = self.midiOut.get_ports()
+    print(self.availableOutputPorts)
+    if len(self.availableOutputPorts) > 0:
+      self.midiOut.open_port(1)
+>>>>>>> ecc5e9f (Added autoconnect for midi too, have not tested yet)
   
   def __combineCommandAndChannel(self, command, channel):
     return ((command & 0xf0) | (channel & 0xf))
