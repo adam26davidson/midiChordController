1fc814e78140dff4e94365f1d0008c239b6d5ca2
Adam Davidson <adam26davidson@gmail.com>
2023-02-03 17:46:19 -0500

    working on adding settings

--- display/settingsMenu/__init__.py
+++ display/settingsMenu/__init__.py
@@ -7,7 +7,16 @@ class SettingsMenuFrame(tk.Frame):
     height = 480
     width = 800
 
-    def __init__(self, container):
-        super().__init__(container, width=self.width, height=self.height, highlightthickness=0, relief="flat", bg="#000000")
-
+    def __init__(self, container, controller):
+        super().__init__(
+            container=container, 
+            width=self.width, 
+            height=self.height, 
+            highlightthickness=0, 
+            relief="flat", 
+            bg="#000000"
+        )
+        self.performButton = tk.Button(self, height=20, width=20, text='TEST', bg='#ffffff')
+        self.performButton.pack()
+        self.controller = controller
         self.pack()
