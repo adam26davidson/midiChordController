6539ddf2a3a69c1a9447326f9081cfaede6134c2
Adam Davidson <adam26davidson@gmail.com>
2023-02-02 19:35:50 -0500

    working on adding settings

--- controllerManager/controller.py
+++ controllerManager/controller.py
@@ -75,7 +75,7 @@ class Controller(ABC):
         self.processButtonEvent(event, control)
       elif control['type'] == 'ANALOG':
         self.processAnalogEvent(event, control, controlState)
-    elif (event.code not in ['04', '03', '01', '05']): # just for testing take this out
+    elif (event.code not in [4, 3, 1, 5]): # just for testing take this out
       print(event)
 
 
