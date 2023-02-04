99aa0a452b9c034f438784677979ae661163edda
Adam Davidson <adam26davidson@gmail.com>
2023-02-04 14:58:28 -0500

    working on adding settings

--- display/settingsMenu/__init__.py
+++ display/settingsMenu/__init__.py
@@ -18,7 +18,12 @@ class SettingsMenuFrame(tk.Frame):
         self.performButton = MenuItem(self, "PERFORM")
         self.performButton.place(relx=0.5, rely=0.5, anchor='center')
 
-        self.midiButton = MenuItem(self, "MIDI SETTINGS")
+        self.midiButton = MenuItem(self, "MIDI")
         self.midiButton.place(relx=0.5, rely=0.2, anchor='center')
+
+        self.chordButton = MenuItem(self, "CHORD")
+        self.chordButton.place(relx=0.5, rely=0.8, anchor='center')
+
+        self.performButton.focus_set()
 
         self.grid(row=0, column=0, sticky='nsew')
