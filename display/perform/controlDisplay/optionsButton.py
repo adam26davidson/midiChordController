6ef859685223c109cec18359fdfa43053ef55e8a
Adam Davidson <adam26davidson@gmail.com>
2023-08-30 21:17:25 -0400

    working on controller map

--- display/perform/controlDisplay/optionsButton.py
+++ display/perform/controlDisplay/optionsButton.py
@@ -20,30 +20,31 @@ class OptionsButton(ControlButton):
         self.drawButton()
 
     def drawButton(self):
+        uToC = self.master.unitsToCoord
         r = (1 - self.xOffset)
         arc1x1 = self.centerX - r
         arc1y1 = self.centerY - 1
         arc1x2 = self.centerX + r
         arc1y2 = arc1y1 + (2*r)
-        self.arc1 = self.master.create_arc(arc1x1, arc1y1, arc1x2, arc1y2, start=0, extent=180, fill="", outline=INACTIVE_COLOR, width=2)
+        self.arc1 = self.master.create_arc(uToC(arc1x1), uToC(arc1y1), uToC(arc1x2), uToC(arc1y2), start=0, extent=180, fill="", outline=INACTIVE_COLOR, width=2)
         
         arc2x1 = self.centerX - r
         arc2y1 = (self.centerY + 1) - (2*r)
         arc2x2 = self.centerX + r
         arc2y2 = arc2y1 + (2*r)
-        self.arc2 = self.master.create_arc(arc2x1, arc2y1, arc2x2, arc2y2, start=180, extent=0, fill="", outline=INACTIVE_COLOR, width=2)
+        self.arc2 = self.master.create_arc(uToC(arc2x1), uToC(arc2y1), uToC(arc2x2), uToC(arc2y2), start=180, extent=0, fill="", outline=INACTIVE_COLOR, width=2)
 
         l1x = self.centerX - r
         ly1 = (self.centerY - 1) + r
         ly2 = (self.centerY + 1) - r
         l2x = self.centerX + r
 
-        self.line1 = self.master.create_line(l1x, ly1, l1x, ly2, fill=INACTIVE_COLOR, width=2)
-        self.line2 = self.master.create_line(l2x, ly1, l2x, ly2, fill=INACTIVE_COLOR, width=2)
+        self.line1 = self.master.create_line(uToC(l1x), uToC(ly1), uToC(l1x), uToC(ly2), fill=INACTIVE_COLOR, width=2)
+        self.line2 = self.master.create_line(uToC(l2x), uToC(ly1), uToC(l2x), uToC(ly2), fill=INACTIVE_COLOR, width=2)
 
         self.textObject = self.master.create_text(
-            self.centerX,
-            self.centerY,
+            uToC(self.centerX),
+            uToC(self.centerY),
             text=self.label,
             fill="#ffffff",
             font=FONT
