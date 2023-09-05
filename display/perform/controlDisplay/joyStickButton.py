c2a89e7c2c0b40a499f198f6b1f6f58f8b268415
Adam Davidson <adam26davidson@gmail.com>
2023-09-05 14:02:34 -0400

    working on controller map

--- display/perform/controlDisplay/joyStickButton.py
+++ display/perform/controlDisplay/joyStickButton.py
@@ -4,7 +4,8 @@ from .constants import ACTIVE_COLOR, INACTIVE_COLOR, FONT
 
 class JoyStickButton(ControlButton):
 
-    radius = 1.5
+    radius = 1.4
+    textRadius = 0.5
 
     def __init__(self, master, lLabel, rLabel, tLabel, bLabel, centerX, centerY):
         super().__init__(master)
@@ -31,35 +32,30 @@ class JoyStickButton(ControlButton):
             width=2
         )
 
-        self.rightTextObject = self.master.create_text(
-            uToC(self.centerX + self.radius),
-            uToC(self.centerY),
-            text=self.rightLabel,
+        self.rightTextBackgroundObject , self.rightTextObject = self.drawTextAndBackground(self.rightLabel, self.centerX + self.radius, self.centerY)
+        self.leftTextBackgroundObject , self.leftTextObject = self.drawTextAndBackground(self.leftLabel, self.centerX - self.radius, self.centerY)  
+        self.topTextBackgroundObject , self.topTextObject = self.drawTextAndBackground(self.topLabel, self.centerX, self.centerY - self.radius)
+        self.bottomTextBackgroundObject , self.bottomTextObject = self.drawTextAndBackground(self.bottomLabel, self.centerX, self.centerY + self.radius)
+
+    def drawTextAndBackground(self, label, centerX, centerY):
+        uToC = self.master.unitsToCoord
+        backgroundObject = self.master.create_oval(
+            uToC(centerX - self.textRadius),
+            uToC(centerY - self.textRadius),
+            uToC(centerX + self.textRadius),
+            uToC(centerY + self.textRadius),
+            fill="#000000",
+            outline=INACTIVE_COLOR,
+            width=2
+        )
+
+        textObject = self.master.create_text(
+            uToC(centerX),
+            uToC(centerY),
+            text=label,
             fill="#ffffff",
             font=FONT
         )
 
-        self.leftTextObject = self.master.create_text(
-            uToC(self.centerX - self.radius),
-            uToC(self.centerY),
-            text=self.leftLabel,
-            fill="#ffffff",
-            font=FONT
-        )
-
-        self.topTextObject = self.master.create_text(
-            uToC(self.centerX),
-            uToC(self.centerY - self.radius),
-            text=self.topLabel,
-            fill="#ffffff",
-            font=FONT
-        )
-
-        self.bottomTextObject = self.master.create_text(
-            uToC(self.centerX),
-            uToC(self.centerY + self.radius),
-            text=self.bottomLabel,
-            fill="#ffffff",
-            font=FONT
-        )
+        return backgroundObject, textObject
     
