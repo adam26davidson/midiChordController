6936fc0f1866c98a10e7effc406d59c2adde5e7d
Adam Davidson <adam26davidson@gmail.com>
2023-09-05 17:14:59 -0400

    working on controller map

--- display/perform/controlDisplay/triggerButton.py
+++ display/perform/controlDisplay/triggerButton.py
@@ -22,26 +22,26 @@ class TriggerButton(ControlButton):
         r = self.radius
 
         xl = self.centerX - (self.width / 2)
-        xm1 = xl + (2*r)
-        xm2 = (self.centerX + (self.width / 2)) - (2*r)
-        xr = xm2 + (2*r)
+        xm1 = xl + r
+        xm2 = (self.centerX + (self.width / 2)) - r
+        xr = self.centerX + (self.width / 2)
 
         yt = self.centerY - (self.height / 2)
-        ym = yt + (2*r)
-        yb = self.centerX + (self.height / 2)
+        ym = yt + r
+        yb = self.centerY + (self.height / 2)
 
         self.arc1 = self.master.create_arc(
             uToC(xl), uToC(yt), 
-            uToC(xm1), uToC(ym), 
-            start=90, extent=180, 
+            uToC(xm1), uToC(ym + r), 
+            start=90, extent=90, 
             fill="", 
             outline=INACTIVE_COLOR, 
             width=2, 
             style="arc")
         
         self.arc2 = self.master.create_arc(
-            uToC(xm2), uToC(yt), 
-            uToC(xr), uToC(ym), 
+            uToC(xm2 - r), uToC(yt), 
+            uToC(xr), uToC(ym + r), 
             start=0, extent=90, 
             fill="", 
             outline=INACTIVE_COLOR, 
