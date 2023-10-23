105aa8186bff0d60c50910c6bddd75f760db7a04
Adam Davidson <adam26davidson@gmail.com>
2023-10-23 19:04:54 -0400

    debugging changes

--- controllerManager/controller.py
+++ controllerManager/controller.py
@@ -272,7 +272,6 @@ class Controller(ABC):
         value: float = None):
 
         mappableEvent = control.getMappableControlEvent(mappableControlType, rawEvent)
-        print(f"sendEvents for {control.label}: {mappableEvent}")
         keys = control.getMappableControlKeys(mappableControlType, rawEvent)
 
         for key in keys:
