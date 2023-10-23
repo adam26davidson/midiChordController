8eb5e9376e2198edf0a93a4f7f8eb6a700b127e8
Adam Davidson <adam26davidson@gmail.com>
2023-10-23 14:23:27 -0400

    debugging changes

--- controllerManager/__init__.py
+++ controllerManager/__init__.py
@@ -10,57 +10,62 @@ import asyncio
 
 class ControllerManager():
   
-  connectedControllers: List[Controller]
+    connectedControllers: List[Controller]
 
-  def __init__(self):
-    self.controllerClasses = controllerClasses
-    self.connectedControllers = []
-    self.subscriberCallbacks = []
-    store.subscribe(self.handleStoreUpdate)
+    def __init__(self):
+        self.controllerClasses = controllerClasses
+        self.connectedControllers = []
+        self.subscriberCallbacks = []
+        store.subscribe(self.handleStoreUpdate)
   
-  def start(self):
-    asyncio.ensure_future(self.checkConnection())
+    def start(self):
+        asyncio.ensure_future(self.checkConnection())
 
-  def subscribe(self, callBack):
-    self.subscriberCallbacks.append(callBack)
+    def subscribe(self, callBack):
+        self.subscriberCallbacks.append(callBack)
 
-  def sendEvent(self, event):
-    for callBack in self.subscriberCallbacks:
-      callBack(event)
+    def sendEvent(self, event):
+        for callBack in self.subscriberCallbacks:
+            callBack(event)
 
-  async def waitForConnection(self, sleepTime=0.25):
-    store.dispatch(actions.startWaitingForConnection())
-    foundController = False
-    connectedController = None
-    while (not foundController):
-      for controller in self.controllerClasses:
-        foundController = controller.checkForNewConnections()
-        if foundController:
-          connectedController: Controller = controller(self.sendEvent)
-          connectedController.open()
-          self.connectedControllers.append(connectedController)
-          store.dispatch(actions.stopWaitingForConnection())
+    async def waitForConnection(self, sleepTime=0.25):
+        store.dispatch(actions.startWaitingForConnection())
+        foundController = False
+        connectedController = None
+        print("entering controller search loop")
+        while (not foundController):
+            print("checking for new connections")
+            for controller in self.controllerClasses:
+                foundController = controller.checkForNewConnections()
+                if foundController:
+                    print("found new controller")
+                    connectedController: Controller = controller(self.sendEvent)
+                    connectedController.open()
+                    self.connectedControllers.append(connectedController)
+                    store.dispatch(actions.stopWaitingForConnection())
 
-          controls = store.get_state()['controllerCoupler']['controls']
-          store.dispatch(ccActions.updateControls({**controls, **connectedController.getControls()}))
-          break
-        await asyncio.sleep(sleepTime)
+                    controls = store.get_state()['controllerCoupler']['controls']
+                    store.dispatch(ccActions.updateControls({**controls, **connectedController.getControls()}))
+                    break
+                else:
+                    print("no new controller found")
+            await asyncio.sleep(sleepTime)
 
-  async def checkConnection(self):
-    while True:
-      self.connectedControllers = self.getUpdatedConnectedControllersList()
-      if not self.connectedControllers:
-        await self.waitForConnection()
-      await asyncio.sleep(0.25)
+    async def checkConnection(self):
+        while True:
+            self.connectedControllers = self.getUpdatedConnectedControllersList()
+            if not self.connectedControllers:
+                await self.waitForConnection()
+            await asyncio.sleep(0.25)
   
-  def getUpdatedConnectedControllersList(self):
-    newConnectedControllerList = []
-    for controller in self.connectedControllers:
-      if controller.checkIfStillConnected():
-        newConnectedControllerList.append(controller)
-      else:
-        controller.close()
-    return newConnectedControllerList
+    def getUpdatedConnectedControllersList(self):
+        newConnectedControllerList = []
+        for controller in self.connectedControllers:
+            if controller.checkIfStillConnected():
+                newConnectedControllerList.append(controller)
+            else:
+                controller.close()
+        return newConnectedControllerList
 
-  def handleStoreUpdate(self):
-    pass
+    def handleStoreUpdate(self):
+        pass
