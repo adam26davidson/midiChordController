03845f4f8c023da8ba6fc6dd4e09d2db42cc8da8
Adam Davidson <adam26davidson@gmail.com>
2023-10-24 16:20:34 -0400

    debugging changes

--- display/displayConstants.py
+++ display/displayConstants.py
@@ -1,8 +1,16 @@
+# COLORS = {
+#   "chordDim": "#828282",
+#   "rootDim": "#bf9536",
+#   "chord": "#ffffff",
+#   "root": "#ffb102",
+#   "darkGrey": "#333333"
+# }
+
 COLORS = {
   "chordDim": "#828282",
-  "rootDim": "#bf9536",
+  "rootDim": "#6d4c7d",
   "chord": "#ffffff",
-  "root": "#ffb102",
+  "root": "#de9cff",
   "darkGrey": "#333333"
 }
 
